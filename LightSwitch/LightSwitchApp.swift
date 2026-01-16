d6765b5ee64929f0020bfc8ad2a3a90caea07a42
Anubhav <programmingwithanubhav@gmail.com>
2026-01-16 18:48:18 +0530

    Initial Commit

---
+++ LightSwitch/LightSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightSwitchApp.swift
+//  LightSwitch
+//
+//  Created by Anubhav Dubey on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LightSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
